812d777e935f7ff50aefb68c36bf6477f1db01b5
MohammadAlzuabi <115696615+MohammadAlzuabi@users.noreply.github.com>
2022-12-22 18:41:07 +0100

    First commit

---
+++ ResturangBokning/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ResturangBokning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
